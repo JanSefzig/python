c8b65c7de158f8969b4af0b7e93152a19fe9f025
JanSefzig <nicolas.jan.sefzig@student.ossp.cz>
2022-09-07 14:49:17 +0200

    c

---
+++ python.py
@@ -0,0 +1,14 @@
+a = 3333
+b = 20022
+if b > a:
+  print("how many ciphers has a")
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+def listOfOddNumbers(a,b):
+    if a % 2 == 0:
+        a = a + 1
+    odds = list(range(a,b,2))
+    return odds
